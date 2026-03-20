9628a08a6be758cfffcac18ce444ac1147890299
Julianne Weiß <julianne.weisz@gmail.com>
2026-03-20 17:55:35 +0100

    Make input from stdin work properly

--- A1/juwei95-WordCount.py
+++ A1/juwei95-WordCount.py
@@ -1,23 +1,40 @@
+#!/bin/python3
+
 import sys
 import re
 
-filename = sys.argv[-1]
+filename = None
+print_list = False
+case_insensitive = False
 
-with open(filename) as f:
-    lines = f.readlines()
+del (sys.argv[0])
+for arg in sys.argv:
+    if "-l" == arg:
+        print_list = True
+    elif "-I" == arg:
+        case_insensitive = True
+    else:
+        filename = arg
+
+if filename == None:
+    file = sys.stdin
+else:
+    file = open(filename)
+
+with file:
     hist = {}
-    for line in lines:
+    for line in file:
         words = re.split(r"[\W]", line)
         words = list(filter(lambda word: word != "", words))
         for word in words:
-            if "-I" in sys.argv:
+            if case_insensitive:
                 word = word.lower()
             if word in hist:
                 hist[word] += 1
             else:
                 hist[word] = 1
     hist = dict(sorted(hist.items(), key=lambda item: (-item[1], item[0])))
-    if "-l" in sys.argv:
+    if print_list:
         for key, value in hist.items():
             print(f"{key}\t{value}")
     else:        
