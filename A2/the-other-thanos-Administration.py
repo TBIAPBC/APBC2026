d2f3e99f87e588a5c7649f0a1d719f188be40674
the-other-thanos <thanos.gramosis@gmail.com>
2026-03-25 22:50:06 +0100

    added flag to print minimum cost and which partition has min cost

--- A2/the-other-thanos-Administration.py
+++ A2/the-other-thanos-Administration.py
@@ -14,18 +14,18 @@ def read_data(filename):
     
     idx = 2
     municipality_cost = {}
+    minimum_additional_cost=number_of_cities//2-1
 
     while idx <= number_of_cities + 1:
         row = re.findall(r"-|\d+", lines[idx].strip())
-        row_city = city_list[idx - 2]
+        city_i = city_list[idx - 2]
         
-        for col_city, cost in zip(city_list, row):
+        for city_j, cost in zip(city_list, row):
             if cost != "-":
-                cost = int(cost)
-                if cost < budget and row_city < col_city:
-                    municipality_cost[(row_city, col_city)] = cost
-        idx += 1
-           
+                cost = int(cost) 
+                if cost <= budget-minimum_additional_cost and city_i < city_j:
+                    municipality_cost[(city_i, city_j)] = cost
+        idx += 1 
     return (municipality_cost,city_list,budget)    
 
 def find_pairings(unpaired_cities, municipality_cost, budget, current_pairs=None, current_cost=0):
@@ -33,26 +33,26 @@ def find_pairings(unpaired_cities, municipality_cost, budget, current_pairs=None
         current_pairs=[]
     if not unpaired_cities:
         return [current_pairs]  
-    else:
-        opt_completion_cost=(len(unpaired_cities)//2)-1
-     
+    
+    minimum_cost=min(municipality_cost.values())
+    minimum_additional_cost=((len(unpaired_cities)//2)-1)*minimum_cost
+
     results=[]
     first_unpaired = unpaired_cities[0]
 
     for i in range(1, len(unpaired_cities)):
         second_unpaired = unpaired_cities[i]
         pair = (first_unpaired, second_unpaired)
-
+        
         if pair in municipality_cost:
-            cost = municipality_cost.get(pair)
-            updated_cost=cost+current_cost+opt_completion_cost
-            if updated_cost > budget:
+            new_cost = municipality_cost.get(pair)+current_cost
+           
+            if new_cost > budget - minimum_additional_cost:
                 continue  
             
             new_remaining = unpaired_cities[1:i] + unpaired_cities[i+1:]
-        
-            results+=find_pairings(new_remaining,municipality_cost,budget,current_pairs + [pair],current_cost + cost)
 
+            results+=find_pairings(new_remaining, municipality_cost, budget, current_pairs + [pair], new_cost)
     return results        
 
 def find_minimum(possible_municipalities, municipality_cost, budget):
@@ -71,21 +71,29 @@ def find_minimum(possible_municipalities, municipality_cost, budget):
 
 
 if __name__=="__main__":
+    
     parser = argparse.ArgumentParser(description='Lists all ways to partition the provinces within a specific budget')
     parser.add_argument('filename', type=str, help='The name of the file containing the adjacency matrix and budget limit.')
     parser.add_argument('-o', action="store_true", help="Prints the lowest total cost instead of a list of possible partitions")
+    parser.add_argument('-op', action="store_true", help="Prints the lowest total cost and which partition/s are possible at such cost")
     args=parser.parse_args()
 
     
     data=read_data(args.filename)
     partitions = find_pairings(data[1], data[0], data[2])
-         
+    min_cost=find_minimum(partitions,data[0],data[2])
     if args.o:    
-        min_cost=find_minimum(partitions,data[0],data[2])
-        print(f'Minimum partitioning cost={min_cost[0]}')
-        #for partition: {min_cost[1]}')  #finish this with another flag to print which partition/s are the cheapest.
-    else:
+        print(f'Minimum partitioning cost = {min_cost[0]}')
+    elif args.op:
+        print(f'Minimum partitioning cost = {min_cost[0]}')
+        print(f'Partitions possible at this cost:')
+        for partition in min_cost[1]:
+            for i in partition:
+                print(f'{i[0]+i[1]} ', end="")
+            print("")
+    else:  
         for partition in partitions:
             for i in partition:
                 print(f'{i[0]+i[1]} ', end="")     
             print("")  
+            
