322f82dae49800e4cbd323c08d3eaea9f2291eb6
BeatriceHN <beatrice.hurbean@gmail.com>
2026-06-07 23:40:03 +0200

    New bot that can beat our old bot. Switches between 3 strategies. It has an early game strategy, a recovery strategy and the old rivalry strategy.

--- A5-Game/Game/runRobotRace.py
+++ A5-Game/Game/runRobotRace.py
@@ -20,10 +20,10 @@ parser.add_argument('--allow_jumps', help="allow players to jump over walls by r
 
 args = parser.parse_args()
 
-robot_module_names = {"Test":"test-RobotRace",
-					"Beatme": "beatme-RobotRace",
-					"AdlhartmNaive": "adlhartm-RobotRace",
-					"RivalrySprint": "rivalrysprint-RobotRace"}
+robot_module_names = {"Adlhartm": "adlhartm-RobotRace",
+					"StalkerHunter": "stalkerhunter_stats-RobotRace",
+					"BeatMe":"beatme-RobotRace",
+                    "Test":"test-RobotRace",}
 
 robotmodules = { m:__import__(m) for m in robot_module_names.values() }
 
@@ -34,9 +34,19 @@ else:
 
 sim = Simulator(map=m, vizfile=args.viz, framerate=args.framerate)
 
+#TODO remove active players
+active_players = []
 for name,module_name in robot_module_names.items():
 	for p in robotmodules[module_name].players:
 		p.player_modname = name
 		sim.add_player(p)
+		active_players.append(p)
+
 
 sim.play(rounds=args.number, jumps_allowed=args.allow_jumps, mine_mode=args.mine_mode.lower())
+
+
+#TODO remove this later
+for p in active_players:
+    if hasattr(p, 'game_over'):
+        p.game_over()
